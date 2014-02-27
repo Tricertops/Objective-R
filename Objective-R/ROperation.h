6167895c9634d40dcd4f7b6eadb5016aa729aa67
Martin Kiss <martin.kiss@me.com>
2014-02-27 20:45:10 +0100

    Declare basic set of ROperators

--- Objective-R/ROperation.h
+++ Objective-R/ROperation.h
@@ -8,6 +8,50 @@
 
 #import "RExpression.h"
 
+
+
+typedef enum ROperator : NSUInteger ROperator;
+
+
+
+
+
 @interface ROperation : RExpression
 
+
++ (instancetype)unary:(ROperator)operator of:(RExpression *)operand;
++ (instancetype)left:(RExpression *)operand binary:(ROperator)operator right:(RExpression *)operand;
++ (instancetype)multiary:(ROperator)operator operands:(RExpression *)operand, ... NS_REQUIRES_NIL_TERMINATION;
+
+@property (atomic, readwrite, assign) ROperator operator;
+@property (atomic, readwrite, copy) NSArray *operands;
+
+
 @end
+
+
+
+
+
+enum ROperator : NSUInteger {
+    
+    ROperatorPlus,      // 0+x, x+y, (x+y)+z
+    ROperatorMinus,     // 0-x, x-y, (x-y)-z
+    ROperatorMultiply,  // x*1, x*y, (x*y)*z
+    ROperatorDivide,    // x/1, x/y, (x/y)/z
+    ROperatorModulo,    // x%2, x%y, (x%y)%z
+    
+    ROperatorEqualTo,   // x=x, x=y, (x=y)&(y=z)
+    ROperatorUnequal,   // x!x, x!y, (x!y)|(y!z)
+    ROperatorLessThan,  // x<x, x<y, (x<y)&(y<z)
+    ROperatorGreater,   // x>x, x>y, (x>y)&(y>z)
+    ROperatorLessThanOrEqual,// <=
+    ROperatorGreaterThanOrEqual,// >=
+    
+    ROperatorNot,       // !x, x!y, (x!y)|(y!z)
+    ROperatorAnd,       // x&x, x&y, (x&y)&z
+    ROperatorOr,        // x|x, x|y, (x|y)|z
+};
+
+
+
--- Objective-R/ROperation.m
+++ Objective-R/ROperation.m
@@ -3,11 +3,69 @@
 //  Objective-R
 //
 //  Created by Martin Kiss on 27.2.14.
-//  Copyright (c) 2014 Triceratops Software, s.r.o. All rights reserved.
+//  Copyright (c) 2014 Triceratops Software:s.r.o. All rights reserved.
 //
 
 #import "ROperation.h"
 
+
+
+
+
 @implementation ROperation
 
+
+
+
++ (instancetype)unary:(ROperator)operator of:(RExpression *)operand {
+    ROperation *operation = [[ROperation alloc] init];
+    operation.operator = operator;
+    operation.operands = @[ operand ];
+    return operation;
+}
+
+
++ (instancetype)left:(RExpression *)left binary:(ROperator)operator right:(RExpression *)right {
+    ROperation *operation = [[ROperation alloc] init];
+    operation.operator = operator;
+    operation.operands = @[ left, right ];
+    return operation;
+}
+
+
++ (instancetype)multiary:(ROperator)operator operands:(RExpression *)operand, ... NS_REQUIRES_NIL_TERMINATION {
+    ROperation *operation = [[ROperation alloc] init];
+    operation.operator = operator;
+    operation.operands = RVariadicArray(operand);
+    return operation;
+}
+
+
++ (NSArray *)stringsForOperator:(ROperator)operator {
+    switch (operator) {
+        case ROperatorPlus:     return @[ @"+", @"plus", @"add", @"positive" ];
+        case ROperatorMinus:    return @[ @"−", @"-", @"minus", @"subtract", @"subtraction", @"negative" ];
+        case ROperatorMultiply: return @[ @"×", @"*", @"multiply by", @"multiplication" ];
+        case ROperatorDivide:   return @[ @"÷", @"/", @"div", @"divide by", @"division" ];
+        case ROperatorModulo:   return @[ @"mod", @"%", @"modulo", @"rem", @"remainder" ];
+            
+        case ROperatorEqualTo:  return @[ @"=", @"≟", @"==", @"===", @"eq", @"equal to", @"equality" ];
+        case ROperatorUnequal:  return @[ @"≠", @"!=", @"ne", @"not equal to", @"inequality" ];
+        case ROperatorLessThan: return @[ @"<", @"lt", @"less than" ];
+        case ROperatorGreater:  return @[ @">", @"gt", @"greater than" ];
+        case ROperatorLessThanOrEqual:      return @[ @"≤", @"<=", @"le", @"less than or equal to" ];
+        case ROperatorGreaterThanOrEqual:   return @[ @"≥", @">=", @"ge", @"greater than or equal to" ];
+            
+        case ROperatorNot:  return @[ @"¬", @"!", @"not", @"negate", @"negation" ];
+        case ROperatorAnd:  return @[ @"∧", @"&", @"·", @"and", @"conjunction" ];
+        case ROperatorOr:   return @[ @"∨", @"|", @"or", @"disjunction" ];
+    }
+}
+
+
+
+
+
 @end
+
+
