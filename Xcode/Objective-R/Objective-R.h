8d90e231c3d34fdb737b69da422dc115444821e1
Martin Kiss <martin.kiss@me.com>
2014-02-27 16:35:41 +0100

    Create project with target

---
+++ Xcode/Objective-R/Objective-R.h
@@ -0,0 +1,11 @@
+//
+//  Objective-R.h
+//  Objective-R
+//
+//  Created by Martin Kiss on 27.2.14.
+//  Copyright (c) 2014 Triceratops Software, s.r.o. All rights reserved.
+//
+
+
+
+
---
+++ Xcode/Objective-R/RThing.h
@@ -0,0 +1,13 @@
+//
+//  RThing.h
+//  Objective-R
+//
+//  Created by Martin Kiss on 27.2.14.
+//  Copyright (c) 2014 Triceratops Software, s.r.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RThing : NSObject
+
+@end
